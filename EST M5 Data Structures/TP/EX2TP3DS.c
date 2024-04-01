01fdff5fd56cf3523238ac14830bd2d7d8ea9fc2
youssef hachimi <83027476+youssef-hachimi@users.noreply.github.com>
2024-04-01 11:59:06 +0000

    Update EX2TP3DS.c

--- EST M5 Data Structures/TP/EX2TP3DS.c
+++ EST M5 Data Structures/TP/EX2TP3DS.c
@@ -1,3 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+typedef struct prior
+{
+    int valeur;
+    int prioritaire;
+    struct prior *next;
+} prioritaire;
+typedef struct
+{
+    prioritaire *sommet;
+} Pile;
+void initialiser(Pile *pile)
+{
+    pile->sommet = NULL;
+}
+void ajouter(Pile *pile){
+    int valeur;
+    prioritaire* newprior = (prioritaire*)malloc(sizeof(prioritaire));
+    newprior->valeur = valeur;
+    newprior->prioritaire = prioritaire;
+}
+void retirer(){}
